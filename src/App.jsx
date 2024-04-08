4b31e7e9f1087d675967a35cf53f5e142a6d5a06
Hemyulin <daniel.budiansky@protonmail.com>
2024-04-08 12:36:26 +0200

    404 Page done

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import SignUpPage from "./pages/SignUpPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { OwnProfilePage } from "./pages/OwnProfilePage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<SignUpPage />} />
+        <Route path="/" element={<OwnProfilePage />} />
       </Routes>
       <Footer />
     </div>
