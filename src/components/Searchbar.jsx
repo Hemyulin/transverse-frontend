fb2b5fd4c2708ebd9fb17b5f512e5d0e0115b794
Piero Schenato <piero.schenato@gmail.com>
2024-04-08 16:46:11 +0200

    saved

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+function Searchbar ({ onSearch }) {
+    const [query, setQuery] = useState('');
+
+    const handleChange = (e) => {
+      setQuery(e.target.value);
+    }
+    const handleSearch = (e) => {
+      e.preventDefault();
+      onSearch(query)
+    }
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <img src="LOGOTOBEFOUND" alt="app logo"/> 
+      </div>
+      <div className="navbar-center">
+       
+        <form onSubmit={handleSearch}>
+          <input type="text" placeholder="Search!" value={query} onChange={handleChange}/>
+          <button type="submit">Search</button>
+          </form>
+      </div>
+      <div className="navbar-right">
+        
+        <button> Register here </button>
+      </div>
+    </div>
+  );
+}
