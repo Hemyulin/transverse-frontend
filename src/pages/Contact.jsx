368194deb20b0b89f086547aae346e322f4b5fe6
Memoryman2022 <rlacannon@gmail.com>
2024-04-08 22:04:26 +0200

    contact template

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import "./Contact.css";
 const ContactPage = () => {
   const navigate = useNavigate();
 
-  return <div>Contact us</div>;
+  return (
+    <div className="Contact-page">
+      <h2>Contact us</h2>
+      <h3>Github links</h3>
+      <br />
+      <h3>LinkedIn links</h3>
+      <br />
+      <h3>Email links</h3>
+    </div>
+  );
 };
 
 export default ContactPage;
