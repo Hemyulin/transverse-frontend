f4bc3b4ca1f1d3f7cc378b2a70964fb541e54f2d
Hemyulin <daniel.budiansky@protonmail.com>
2024-04-08 12:24:40 +0200

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
-
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import SignUpPage from "./pages/SignUpPage";
 
 function App() {
