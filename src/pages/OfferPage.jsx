238f6c3341311d3951abb2591a41f754904feba6
Hemyulin <daniel.budiansky@protonmail.com>
2024-04-09 16:46:20 +0200

    minor change

--- src/pages/OfferPage.jsx
+++ src/pages/OfferPage.jsx
@@ -1,42 +1,43 @@
-import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom'; // react router have?
-import Footer from '../components/Footer';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom"; // react router have?
+import Footer from "../components/Footer";
 
-const API_URL = process.env.NODE_ENV === 'https://transverse.adaptable.app/' || 'http://localhost:5005';//doublecheck thet
+const API_URL =
+  // process.env.NODE_ENV ===
+  "https://transverse.adaptable.app" || "http://localhost:5005"; //doublecheck thet
 
 const OfferPage = () => {
-    const { offerId } = useParams();
-    const [offerDetails, setOfferDetails] = useState(null);
+  const { offerId } = useParams();
+  const [offerDetails, setOfferDetails] = useState(null);
 
-
-      useEffect(() => {
-        const getSingleOffer = async () => {
-            try {
-            const res = await fetch(`/api/offers/${offerId}`);
-            const parsed = await res.json();
-            setOfferDetails(parsed);
-            } catch (error) {
-                console.error("could not fetch the offer", error)
-            } 
-          };
-        getSingleOffer();
-      }, [offerId]);
-      if (!offerDetails) {
-        return <div>Loading...</div>
+  useEffect(() => {
+    const getSingleOffer = async () => {
+      try {
+        const res = await fetch(`/api/offers/${offerId}`);
+        const parsed = await res.json();
+        setOfferDetails(parsed);
+      } catch (error) {
+        console.error("could not fetch the offer", error);
       }
-  
-    return (
-        <div>
-            <div className="offer-images">
-                {offerDetails.images.map((imageUrl, index) => (
-                    <img key={index} src={imageUrl} alt={`Image ${index + 1}`} />
-                ))}
-            </div>
-            <div className="offer-accommodation">
-                <h2>{offerDetails.title}</h2>
-                <p>{offerDetails.description}</p>
-            </div>
-            {/* <div className="offer-location">
+    };
+    getSingleOffer();
+  }, [offerId]);
+  if (!offerDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div className="offer-images">
+        {offerDetails.images.map((imageUrl, index) => (
+          <img key={index} src={imageUrl} alt={`Image ${index + 1}`} />
+        ))}
+      </div>
+      <div className="offer-accommodation">
+        <h2>{offerDetails.title}</h2>
+        <p>{offerDetails.description}</p>
+      </div>
+      {/* <div className="offer-location">
                 <h2>Location</h2>
                 <iframe
                     title="Offer Location"
@@ -48,18 +49,18 @@ const OfferPage = () => {
                     allowFullScreen
                 ></iframe>
             </div> */}
-            {/* Host Reviews */}
-            <div className="offer-reviews">
-                <h2>Host Reviews</h2>
-                {offerDetails.comments.map(review => (
-                    <div key={review.id}>
-                        <p>Rating: {review.rating}</p>
-                    </div>
-                ))}
-            </div>
-            <Footer />
-        </div>
-    );
-}
+      {/* Host Reviews */}
+      <div className="offer-reviews">
+        <h2>Host Reviews</h2>
+        {offerDetails.comments.map((review) => (
+          <div key={review.id}>
+            <p>Rating: {review.rating}</p>
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default OfferPage;
