e97e170a2f1c1a29a48ea7ccdbf934b762fcb7a3
Hemyulin <daniel.budiansky@protonmail.com>
2024-04-12 05:54:30 +0200

    about links

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,21 +10,68 @@ const ContactPage = () => {
       <h3>Github - Email - LinkedIn</h3>
       <div className="tab">
         <h4>Robert Cannon</h4>
-        <h4>https://github.com/Memoryman2022</h4>
-        <h4>rlacannon@gmail.com</h4>
+        <h4>
+          <a
+            href="https://github.com/Memoryman2022"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Profile
+          </a>
+        </h4>
+        <h4>
+          <a href="mailto:rlacannon@gmail.com">rlacannon@gmail.com</a>
+        </h4>
       </div>
       <br />
       <div className="tab">
         <h4>Piero Schenato</h4>
-        <h4>piero.schenato@gmail.com</h4>
-        <h4>https://www.linkedin.com/in/piero-schenato-b09766181/</h4>
-        <h4>https://github.com/pieroschenato</h4>
+        <h4>
+          <a
+            href="https://www.linkedin.com/in/piero-schenato-b09766181/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn Profile
+          </a>
+        </h4>
+        <h4>
+          <a
+            href="https://github.com/pieroschenato"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Profile
+          </a>
+        </h4>
+        <h4>
+          <a href="mailto:piero.schenato@gmail.com">piero.schenato@gmail.com</a>
+        </h4>
       </div>
       <br />
       <div className="tab">
-        <h4>Daniel Budinsky</h4>
-        <h4>https://github.com/Hemyulin</h4>
-        <h4>budanielsky@gmail.com</h4>
+        <h4>Daniel Budiansky</h4>
+        <h4>
+          <a
+            href="https://www.linkedin.com/in/daniel-budiansky-fullstack"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn Profile
+          </a>
+        </h4>
+        <h4>
+          <a
+            href="https://github.com/Hemyulin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Profile
+          </a>
+        </h4>
+        <h4>
+          <a href="mailto:budanielsky@gmail.com">budanielsky@gmail.com</a>
+        </h4>
       </div>
       <br />
     </div>
